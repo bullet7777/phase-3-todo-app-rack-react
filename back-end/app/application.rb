fafcf5adca31e5ef189e06aa1f7b866dfe82c5f6
chris <chris.mahfouz@hotmail.com>
2021-06-02 21:46:39 +0300

    commit tree 8fb90f02bd7b8a816e5ff260065c927612d58241

--- back-end/app/application.rb
+++ back-end/app/application.rb
@@ -1,16 +1,35 @@
 require 'pry'
 require 'json'
 
+
 class Application
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/test/) 
 
-      return [200, { 'Content-Type' => 'application/json' }, [ {:message => "response success"}.to_json ]]
+    if req.path.match(/tasks/)
 
+      if req.post?
+        input = JSON.parse(req.body.read)
+        category = Category.find_by(name:input["category"])
+        task = Task.create(text:input["text"],category_id:category.id)
+        return [200, { 'Content-Type' => 'application/json' }, [ task.to_json ]]
+      end
+
+       if req.delete?
+      task_id = req.path.split("/tasks/").last
+      task = Task.find_by(id:task_id.to_i)
+      task.destroy
+      return [200, { 'Content-Type' => 'application/json' }, [ {message:"Delete Successful"}.to_json ]]
+       end
+
+
+
+      tasks=Task.all
+      return [200, { 'Content-Type' => 'application/json' }, [ tasks.to_json ]]
+    
     else
       resp.write "Path Not Found"
 
--- back-end/db/seeds.rb
+++ back-end/db/seeds.rb
@@ -5,9 +5,14 @@ Task.destroy_all
 puts "Seeding Categories..."
 
 # create categories
+category_1=Category.create(name:"Food")
+Category.create(name:"Misc")
+Category.create(name:"Code")
+Category.create(name:"Money") 
 
 puts "Seeding tasks..."
 
 # create tasks
+Task.create(text:"eat healtjhy",category_id:category_1.id)
 
 puts "Done!"
